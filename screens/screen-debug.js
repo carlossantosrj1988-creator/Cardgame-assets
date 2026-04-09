0b5b46c8cd7c276e1cfb34b70709e1c80f136ae3
carlossantosrj1988-creator <carlossantosrj1988@gmail.com>
2026-04-09 07:28:48 -0300

    Delete screens directory

--- screens/screen-debug.js
+++
@@ -1,5 +0,0 @@
-function _updateDebugScreen(){var d=document.getElementById('debug-screen');if(!d)return;var b=window._getGameBase?window._getGameBase():{w:'?',h:'?'};d.innerHTML='iW:'+window.innerWidth+' iH:'+window.innerHeight+'<br>sW:'+screen.width+' sH:'+screen.height+'<br>DPR:'+window.devicePixelRatio+'<br>base:'+b.w+'x'+b.h;}
-_updateDebugScreen();setInterval(_updateDebugScreen,1000);window.addEventListener('resize',_updateDebugScreen);
-</script>
-</body>
-</html>
